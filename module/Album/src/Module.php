424b6c283ee669b61191832a516763a7481f9f16
Lucas Pacheco <lucaspsilveira@live.com>
2019-11-23 00:00:37 -0300

    View Albuns

--- module/Album/src/Module.php
+++ module/Album/src/Module.php
@@ -3,6 +3,9 @@
 namespace Album;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\Db\Adapter\AdapterInterface;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
 
 class Module implements ConfigProviderInterface
 {
@@ -10,4 +13,36 @@ class Module implements ConfigProviderInterface
     {
         return include __DIR__ . '/../config/module.config.php';
     }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+                Model\AlbumTable::class => function($container) {
+                    $tableGateway = $container->get(Model\AlbumTableGateway::class);
+                    return new Model\AlbumTable($tableGateway);
+                },
+                Model\AlbumTableGateway::class => function ($container) {
+                    $dbAdapter = $container->get(AdapterInterface::class);
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Model\Album());
+                    return new TableGateway('album', $dbAdapter, null, $resultSetPrototype);
+                },
+            ],
+        ];
+    }
+
+    public function getControllerConfig()
+    {
+        return [
+            'factories' => [
+                Controller\AlbumController::class => function($container) {
+                    return new Controller\AlbumController(
+                        $container->get(Model\AlbumTable::class)
+                    );
+                },
+            ],
+        ];
+    }
+
 }
